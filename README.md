9dbb4105ad0fc166bf57b2dfda64bf05f88fd0f3
Glen Carney <concarnied@gmail.com>
2016-07-10 10:31:04 -0700

    Got views and pug template working

---
+++ README.md
@@ -0,0 +1,2 @@
+This project's aim is to create an app where sellers can add items to a store, and customers can browse and purchase them.
+
---
+++ app.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+
+var appInit = function(app){
+  app.set('view engine', 'pug');
+  app.set('views', './app/views');
+  app.listen(3000);
+  console.log('Now listening');
+};
+
+var appRoutes = function(app){
+
+    app.get('/', function(req, res){
+      res.render('index');
+    });
+
+    app.get('/other', function(req, res){
+      res.render('other');
+    });
+};
+
+appInit(app);
+appRoutes(app);
